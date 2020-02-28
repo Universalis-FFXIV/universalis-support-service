dcb77a8a9002d99de690369896fc4d8c549fb886
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-02-27 22:59:21 -0800

    change format

--- src/universalis-support-service.ts
+++ src/universalis-support-service.ts
@@ -9,7 +9,7 @@ const { token } = require("../config.json");
 const client = new Discord.Client();
 const logger = Winston.createLogger({
     level: "info",
-    format: Winston.format.colorize(),
+    format: Winston.format.json(),
     transports: [
         new Winston.transports.Console(),
         new Winston.transports.File({
