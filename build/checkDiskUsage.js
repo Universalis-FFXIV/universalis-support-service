00ddf91a0bc6eb19d2ee27ff94b3b8183a4b6097
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-06-02 00:21:39 -0700

    one more test

--- build/checkDiskUsage.js
+++ build/checkDiskUsage.js
@@ -13,8 +13,7 @@ function checkDiskUsage(fn) {
         lines.shift();
         for (const line of lines) {
             const words = line.split(/\s+/g);
-            for (const word of words)
-                console.log(word);
+            console.log(words[4]);
             if (words.length === 0)
                 continue;
             deviceList[words[5]] = parseInt(words[4].substr(0, words[4].length - 1));
--- src/checkDiskUsage.ts
+++ src/checkDiskUsage.ts
@@ -13,8 +13,7 @@ export function checkDiskUsage(fn: (err: Error | null, deviceList: { [key: strin
 		lines.shift();
 		for (const line of lines) {
 			const words = line.split(/\s+/g);
-			for (const word of words)
-				console.log(word);
+			console.log(words[4]);
 			if (words.length === 0)
 				continue;
 			deviceList[words[5]] = parseInt(words[4].substr(0, words[4].length - 1));
