f69194b3a00cc65c36201e01e1f655e2ce8b7364
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-01-08 18:11:38 -0800

    Remove build folder

--- build/types/ReactionAction.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ReactionAction = void 0;
-var ReactionAction;
-(function (ReactionAction) {
-    ReactionAction[ReactionAction["Added"] = 0] = "Added";
-    ReactionAction[ReactionAction["Removed"] = 1] = "Removed";
-})(ReactionAction = exports.ReactionAction || (exports.ReactionAction = {}));
